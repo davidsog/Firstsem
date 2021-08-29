7f9a1ffa95e50c4796c1bdbc873c18cf999309f3
davidsog <89423145+davidsog@users.noreply.github.com>
2021-08-29 13:00:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,94 +4,145 @@
 #include <stdlib.h>
 
 const double delta = 1E-6;
-struct answers{
+ enum NumOfRoots{
+        Zero, 
+        One, 
+        Two, 
+        Infinity 
+    };
+
+enum Comparison{
+        Less, 
+        Equal, 
+        More
+    };
+
+
+struct Solve{
     double root1;
     double root2;
-    int k;
+    enum NumOfRoots k;
 };
 
+#define INIT_Solve(X) Solve X = {.root1 = NAN, .root2 = NAN, .k = Zero}
 
-int Comparison_of_double(double d1, double d2)
+
+enum Comparison Comparison_of_double(double d1, double d2)
 {
     if ( -delta < (d1 - d2) && (d1 - d2) < delta )
-        return 1;
-    return 0;    
+        return Equal;
+    if ( d1 < d2 )
+        return Less;
+    return More;        
 }
 
-double Solution_of_a_lineal_equation(double b, double c)
+double Solve_of_a_linear_equation(double b, double c)
 {
-    return -c/b;
+    if ( Comparison_of_double(b, 0) != Equal)
+        return -c/b;
+    return NAN;    
 }
 
-answers Solution_of_a_quadratic_equation( double a, double b, double c )
+Solve Solve_of_a_quadratic_equation( double a, double b, double c )
 {
     double D = 0;
-    answers roots;
-    roots = {NAN, NAN, 0};
+    //Solve roots;
+    INIT_Solve(roots);
+    //roots = {NAN, NAN, Zero};
+    //roots.root1 = roots.root2 = NAN;
+    //roots.k = Zero;
     // 0 - no roots, 1 - one root, 2 - two roots, 3 - inf of roots
-    if (Comparison_of_double(a, 0))
+    if (Comparison_of_double(a, 0) == Equal)
     {
-        if (Comparison_of_double(b, 0))
-            if (Comparison_of_double(c, 0))
+        if (Comparison_of_double(b, 0) == Equal)
+        {
+            if (Comparison_of_double(c, 0) == Equal)
             {
-                roots.k = 3;
+                roots.k = Infinity;
                 return roots;
             }
-        roots.root1=roots.root2=Solution_of_a_lineal_equation(b, c);
-        roots.k = 1;
+            else 
+            {
+                roots.k = Zero;
+                return roots; 
+            }
+        }
+        roots.root1=roots.root2=Solve_of_a_linear_equation(b, c);
+        roots.k = One;
         return roots;
     }
     D = b * b - 4 * a * c;
-    if (D < 0)
+    if (Comparison_of_double(D, 0) == Less)
     {
-        roots.k = 0;
+        roots.k = Zero;
         return roots;
     }    
-    if (Comparison_of_double(D, 0))
+    if (Comparison_of_double(D, 0) == Equal)
     {
         roots.root1 = roots.root2 = -b/(2*a);
-        roots.k = 1;
+        roots.k = One;
         return roots;
     }
     D = sqrt(D);
     roots.root1 = (-b + D)/(2*a);
     roots.root2 = (-b - D)/(2*a);
-    roots.k = 2;
+    roots.k = Two;
     return roots;
 }
 
-
+//гит клон и адрес
+//гит стадж
+//enum(на счёт к)
 int Quadratic_equation()
 {
     double a = 0, b = 0, c = 0;
-    answers ans;
-    ans.k = ans.root1 = ans.root2 = 0; 
+    INIT_Solve(ans);
+    ans.root1 = ans.root2 = NAN; 
+    ans.k = Zero;
     printf("Enter the coefficients of the quadratic equation a, b, c in this order separated by spaces." 
     "Put numbers and nothig more!"
     "(Everything that you enter after these numbers does not interest the program!)\n");
     if (scanf("%lf %lf %lf", &a, &b, &c) != 3)
         { 
-          printf("Uncorrect\n");
+          printf("Typing error\n");
           return 0;
         }
-    ans = Solution_of_a_quadratic_equation(a, b, c);
-    if ( Comparison_of_double(ans.k, 0) )
+    ans = Solve_of_a_quadratic_equation(a, b, c);
+    if ( ans.k == Zero )
         printf("No roots\n");
-    if ( Comparison_of_double(ans.k, 1) )
+    if ( ans.k == One )
         printf("One root is %lf\n", ans.root1);
-    if ( Comparison_of_double(ans.k, 2) ) 
+    if ( ans.k == Two) 
         printf("Two roots: %lf and %lf\n", ans.root1, ans.root2);   
-    if ( Comparison_of_double(ans.k, 3) )  
+    if ( ans.k == Infinity )  
         printf("Inf of roots\n");      
     return 0;
 }
 
 void unit_test()
 {
-    int k = 0;
-    for(k = 0; k < 100; k++)
-        Solution_of_a_quadratic_equation(rand(), rand(), rand());    
+    INIT_Solve(ans);
+    ans = Solve_of_a_quadratic_equation(1, 1, 1);
+    if ( ans.k != Zero )
+        printf("Error 1 in Unit test!!\n");
+
+    ans = Solve_of_a_quadratic_equation(0, 0, 1);
+    if ( ans.k != Zero )
+        printf("Error 2 in Unit test!!\n");  
+
+    ans = Solve_of_a_quadratic_equation(0, 2, 4);
+    if ( ans.k != One || Comparison_of_double(-2, ans.root1) != Equal)
+        printf("Error 3 in Unit test!!\n");
+
+    ans = Solve_of_a_quadratic_equation(1, -4, 4);
+    if ( ans.k != One || Comparison_of_double(2, ans.root1) != Equal)
+        printf("Error 4 in Unit test!!\n");    
+
+    ans = Solve_of_a_quadratic_equation(1, 0, -1);
+    if ( ans.k != Two || Comparison_of_double(1, ans.root1) != Equal || Comparison_of_double(-1, ans.root2) != Equal )
+        printf("Error 5 in Unit test!!\n");           
 }
+
 
 int main()
 {
